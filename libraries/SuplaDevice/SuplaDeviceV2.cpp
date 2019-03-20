9e60bf38ae78c89195bb77190c194306484914a2
Jakub Biwel <jakub.biwel@nokia.com>
2019-03-20 14:52:17 +0100

    SuplaDeviceV2: POC definig interfaces

--- libraries/SuplaDevice/SuplaDeviceV2.cpp
+++ libraries/SuplaDevice/SuplaDeviceV2.cpp
@@ -1,23 +1,125 @@
 #include "SuplaDeviceV2.h"
 
+#include <Client.h>
 
-SuplaDev::SuplaDev() : mSrpc(NULL) {
+#include "srpc.h"
 
-    memset(&mSuplaDev, 0, sizeof(TDS_SuplaRegisterDevice_C));
+_supla_int_t SuplaClient::read(void *buf, _supla_int_t count) {
+    mClient->available();
+    return 0;
 }
 
-int SuplaDev::begin() {
+_supla_int_t SuplaClient::read(void *buf, _supla_int_t count, void *userData) {
+    SuplaClient *cli = reinterpret_cast<SuplaClient*>(userData);
+    if (cli)
+        return cli->read(buf, count);
+
+    return -1;
+}
+
+
+_supla_int_t SuplaClient::write(void *buf, _supla_int_t count) {
+    return 0;
+}
+
+_supla_int_t SuplaClient::write(void *buf, _supla_int_t count, void *userData) {
+   SuplaClient *cli = reinterpret_cast<SuplaClient*>(userData);
+    if (cli)
+        return cli->write(buf, count);
+
+    return -1;
+}
+
+void SuplaClient::onRemoteCallReceived(void *_srpc, unsigned _supla_int_t rr_id, unsigned _supla_int_t call_type, void *userData, unsigned char proto_version) {
+    SuplaClient *cli = reinterpret_cast<SuplaClient*>(userData);
+    if (cli)
+        return cli->onRemoteCallReceived(_srpc, rr_id, call_type, proto_version);
+
+    return;
+}
+
+void SuplaClient::onRemoteCallReceived(void *_srpc, unsigned _supla_int_t rr_id, unsigned _supla_int_t call_type, unsigned char proto_version) {
+    TsrpcReceivedData rd;
+    char result;
+
+    supla_log(LOG_DEBUG, "Data received. CallType:%i ProtoVer:%i", call_type, proto_version);
+
+    result = srpc_getdata(_srpc, &rd, 0);
+    if (result != SUPLA_RESULT_TRUE) {
+        supla_log(LOG_ERR, "Failed to get data from server. Ret: %i", result);
+        return;
+    }
+
+    switch (rd.call_type) {
+        case SUPLA_SDC_CALL_VERSIONERROR:
+
+            break;
+
+        case SUPLA_SD_CALL_REGISTER_DEVICE_RESULT:
+
+            break;
+
+        case SUPLA_SD_CALL_CHANNEL_SET_VALUE:
+
+            break;
+
+        case SUPLA_SDC_CALL_SET_ACTIVITY_TIMEOUT_RESULT:
+
+            break;
+
+        default:
+            supla_log(LOG_DEBUG, "Unsupported call type");
+            break;
+    }
+
+    srpc_rd_free(&rd);
+}
+
+SuplaClient::SuplaClient(Client *client) : mClient(client), mSrpc(NULL) {
+
+    memset(&mSuplaDevC, 0, sizeof(TDS_SuplaRegisterDevice_C));
+
+		setState(State::UNINITIALIZED);
+}
+
+int SuplaClient::begin() {
     TsrpcParams params;
 
     srpc_params_init(&params);
     params.user_params = this;
-    params.data_read = NULL;
-    params.data_write = NULL;
-    params.on_remote_call_received = NULL;
+    params.data_read = read;
+    params.data_write = write;
+    params.on_remote_call_received = onRemoteCallReceived;
 
-    mSrpc = srpc_init(&params);
-    if (mSrpc == NULL)
+    mSrpc = reinterpret_cast<Tsrpc*>(srpc_init(&params));
+    if (mSrpc == NULL) {
+				setState(State::UNINITIALIZED):
         return -1;
+		}
+
+		setState(State::INITIALIZED);
 
     return 0;
+}
+
+int SuplaClient::addChannel(SuplaChannel *channel) {
+
+		if (getState() != State::UNINITIALIZED) {
+			supla_log(LOG_INFO, "Device already initialized");
+			return -1;
+		}
+
+		if (mSuplaDevC.channel_count == SUPLA_CHANNELMAXCOUNT) {
+			supla_log(LOG_ERR, "MAXCHANNELCOUNT reached");
+			return -1;
+		}
+
+		mSuplaDevC.channels[mSuplaDevC.channel_count].Number = mSuplaDevC.channel_count;
+
+		channel->setClient(this);
+		channel->setNumber(mSuplaDevC.channel_count);
+
+		mSuplaDevC.channel.Number++;
+
+    return -1;
 }
--- libraries/SuplaDevice/SuplaDeviceV2.h
+++ libraries/SuplaDevice/SuplaDeviceV2.h
@@ -1,19 +1,95 @@
 #ifndef __SUPLA_DEV_V2__
 #define __SUPLA_DEV_V2__
 
+#include <list>
 #include "proto.h"
 
-class SuplaDev {
+class SuplaClient;
+class Client;
+struct Tsrpc;
+
+class SuplaChannel {
+
+private:
+    unsigned char mChannelNum;
+
+    _supla_int_t mType;
+
+    SuplaClient *mSuplaClient;
+
+    void setClient(SuplaClient *client) { mSuplaClient = client; }
+
+    void setNumber(unsigned char num) { mChannelNum = num; }
 
 public:
-    SuplaDev();
-    virtual SuplaDev() { }
+    SuplaChannel(_supla_int_t type) { mType = type; }
+
+    virtual ~SuplaChannel() { }
+
+    void setType(_supla_int_t type) { mType = type; }
+
+    _supla_int_t getType() { return mType; }
+
+    unsigned char getChannelNum() { return mChannelNum; }
+
+    virtual int setValue(char value);
+
+    virtual int setValue(double value);
+
+    virtual int onRemoteValueChange(TSD_SuplaChannelNewValue *value) = 0;
+};
+
+
+class SuplaClient {
+public:
+    enum State {
+        UNINITIALIZED = 0,
+        INITIALIZED,
+        REGISTERING,
+        REGISTERED,
+        DISCONNECTED,
+        CONNECTING,
+        CONNECTED,
+    };
+
+
+private:
+    Client *mClient;
+
+    TDS_SuplaRegisterDevice_C mSuplaDevC;
+
+    Tsrpc *mSrpc;
+
+    State mState;
+
+    std::list<SuplaChannel*> mChannels;
+
+    void setState(SuplaClient::State state) { mState = state; }
+
+    _supla_int_t read(void *buf, _supla_int_t count);
+
+    static _supla_int_t read(void *buf, _supla_int_t count, void *userData);
+
+    _supla_int_t write(void *buf, _supla_int_t count);
+
+    static _supla_int_t write(void *buf, _supla_int_t count, void *userData);
+
+    void onRemoteCallReceived(void *_srpc, unsigned _supla_int_t rr_id, unsigned _supla_int_t call_type, unsigned char proto_version);
+
+    static void onRemoteCallReceived(void *_srpc, unsigned _supla_int_t rr_id, unsigned _supla_int_t call_type, void *userData, unsigned char proto_version);
+
+public:
+    SuplaClient(Client *client);
+
+    virtual ~SuplaClient() { }
+
+    SuplaClient::State getState() { return mState; }
 
     int begin();
 
-private:
-    TDS_SuplaRegisterDevice_C mSuplaDev;
-    Tsrpc *mSrpc;
+    int addChannel(SuplaChannel *channel);
+
+    int iterate();
 };
 
 #endif //__SUPLA_DEV_V2__
